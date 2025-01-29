621f50cb0f8c24b35d9ec8190f419773d8f2198e
Neighborkid01 <calebproland@gmail.com>
2025-01-29 20:25:56 +1000

    Updating lint-staged to check TS for compile errs

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,6 @@
 export default {
-  "*.{ts,js,tsx,jsx,mjs,mts}": "eslint --cache --fix",
+  "*.{ts,js,tsx,jsx,mts,mjs}": "eslint --cache --fix",
+  "*.{ts,tsx,mts}": "npx tsc --noEmit",
   "*.rb": (files) => {
     const filtered = files.filter(file => !file.endsWith("db/schema.rb"));
     if (filtered.length === 0) { return "echo \"Have to return something so we don't lint the whole project\""; }
